75629764d6c9f839e070458b50af8b10f164fd26
aaronnorrish <32919148+aaronnorrish@users.noreply.github.com>
2018-07-22 22:17:45 +0100

    L8 calc regression slope (uncleaned data)
    m = 5.07793064344

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -23,18 +23,10 @@ net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 from sklearn.cross_validation import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
-### fill in a regression here!  Name the regression object reg so that
-### the plotting code below works, and you can see what your regression looks like
-
-
-
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print reg.coef_[0][0]
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -68,6 +60,7 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print reg.coef_[0][0]
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
@@ -81,4 +74,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
