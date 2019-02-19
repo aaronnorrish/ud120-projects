aa38e038bb3806079c2d11acc454642a3c5058cc
aaronnorrish <aaron.norrish97@gmail.com>
2019-02-19 14:50:18 +0800

    L14 tree accuracy with training/test set

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,11 +25,14 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
 ### it's all yours from here forward!
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 clf = DecisionTreeClassifier()
-clf = clf.fit(features, labels)
-predicted = clf.predict(features)
-print "accuracy :", accuracy_score(labels, predicted)
+clf = clf.fit(features_train, labels_train)
+predicted = clf.predict(features_test)
+print "accuracy :", accuracy_score(labels_test, predicted)
